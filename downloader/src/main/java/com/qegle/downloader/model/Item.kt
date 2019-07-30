6b7e93dbe70b31cf63aa8c8dd697fa0736306780
Махаев Сергей <qwaqalkaib@gmail.com>
2019-07-30 11:24:51 +0500

    UndeliverableException fixed

--- downloader/src/main/java/com/qegle/downloader/model/Item.kt
+++ downloader/src/main/java/com/qegle/downloader/model/Item.kt
@@ -1,6 +1,9 @@
 package com.qegle.downloader.model
 
-import com.qegle.downloader.*
+import com.qegle.downloader.DownloadFile
+import com.qegle.downloader.ErrorType
+import com.qegle.downloader.LoadStatus
+import com.qegle.downloader.TimingListener
 import io.reactivex.Observable
 import io.reactivex.disposables.Disposable
 import io.reactivex.observers.DisposableObserver
@@ -12,36 +15,36 @@ import java.io.File
  * Интерфейс обертки над загрузкой файла
  */
 interface IItem {
-	/**
-	 * Уникальный идентификатор
-	 */
-	val id: String
+    /**
+     * Уникальный идентификатор
+     */
+    val id: String
 
-	/**
-	 * Ничинает загрузку. Возвращает реультат в $onSuccess
-	 * @param onSuccess - вызывается при успешной загрузке
-	 */
-	fun download(onSuccess: () -> Unit)
+    /**
+     * Ничинает загрузку. Возвращает реультат в $onSuccess
+     * @param onSuccess - вызывается при успешной загрузке
+     */
+    fun download(onSuccess: () -> Unit)
 
-	/**
-	 * Ставит заргузку на паузу
-	 */
-	fun pause()
+    /**
+     * Ставит заргузку на паузу
+     */
+    fun pause()
 
-	/**
-	 * Возобновляет загрузку
-	 */
-	fun resume()
+    /**
+     * Возобновляет загрузку
+     */
+    fun resume()
 
-	/**
-	 * Останавливает загрузку. Без возможности возобновить!!
-	 */
-	fun stop()
+    /**
+     * Останавливает загрузку. Без возможности возобновить!!
+     */
+    fun stop()
 
-	/**
-	 * Проверяет на успешность загрузки текущего item'a
-	 */
-	fun isExist(): Boolean
+    /**
+     * Проверяет на успешность загрузки текущего item'a
+     */
+    fun isExist(): Boolean
 }
 
 /**
@@ -54,86 +57,90 @@ interface IItem {
  */
 class Item(override val id: String, val url: String, var meta: Meta) : IItem {
 
-	var timingListener: TimingListener? = null
-	var status: LoadStatus = LoadStatus.PAUSE
-	var progressSubject: PublishSubject<Int> = PublishSubject.create()
-	var errorSubject: PublishSubject<Pair<ErrorType, String>> = PublishSubject.create()
-	private var loader: DownloadFile? = null
-	private var progress = 0
-	private var loadingDis: Disposable? = null
+    var timingListener: TimingListener? = null
+    var status: LoadStatus = LoadStatus.PAUSE
+    var progressSubject: PublishSubject<Int> = PublishSubject.create()
+    var errorSubject: PublishSubject<Pair<ErrorType, String>> = PublishSubject.create()
+    private var loader: DownloadFile? = null
+    private var progress = 0
+    private var loadingDis: Disposable? = null
 
-	override fun download(onSuccess: () -> Unit) {
-		if (status == LoadStatus.IN_PROGRESS) stop()
+    override fun download(onSuccess: () -> Unit) {
+        if (status == LoadStatus.IN_PROGRESS) stop()
 
-		loadingDis = Observable.create<Int> {
-			loader = DownloadFile(this,
-				onSuccess = { url: String, loading: Long, fileSize: Long ->
-					timingListener?.onLoading(url, loading, fileSize)
-					it.onComplete()
-				},
-				onProgress = { progress -> it.onNext(progress) },
-				onError = { type, message -> it.tryOnError(LoadingException(type, message)) })
-			status = LoadStatus.IN_PROGRESS
-			loader?.load()
-		}
-			.subscribeOn(Schedulers.io())
-			.observeOn(Schedulers.io())
-			.subscribeWith(object : DisposableObserver<Int?>() {
-				override fun onComplete() {
-					status = LoadStatus.COMPLETE
-					onSuccess.invoke()
-				}
+        loadingDis = Observable.create<Int> {
+            loader = DownloadFile(this,
+                onSuccess = { url: String, loading: Long, fileSize: Long ->
+                    timingListener?.onLoading(url, loading, fileSize)
+                    if (it.isDisposed.not()) it.onComplete()
+                },
+                onProgress = { progress ->
+                    if (it.isDisposed.not()) it.onNext(progress)
+                },
+                onError = { type, message ->
+                    if (it.isDisposed.not()) it.tryOnError(LoadingException(type, message))
+                })
+            status = LoadStatus.IN_PROGRESS
+            loader?.load()
+        }
+            .subscribeOn(Schedulers.io())
+            .observeOn(Schedulers.io())
+            .subscribeWith(object : DisposableObserver<Int?>() {
+                override fun onComplete() {
+                    status = LoadStatus.COMPLETE
+                    onSuccess.invoke()
+                }
 
-				override fun onNext(progress: Int) {
-					this@Item.progress = progress
-					progressSubject.onNext(progress)
-				}
+                override fun onNext(progress: Int) {
+                    this@Item.progress = progress
+                    progressSubject.onNext(progress)
+                }
 
-				override fun onError(e: Throwable) {
-					status = LoadStatus.ERROR
-					if (e is LoadingException)
-						errorSubject.onNext(Pair(e.type, e.message ?: ""))
-					else
-						errorSubject.onNext(Pair(ErrorType.UNKNOWN, e.message ?: ""))
-				}
-			})
-	}
+                override fun onError(e: Throwable) {
+                    status = LoadStatus.ERROR
+                    if (e is LoadingException)
+                        errorSubject.onNext(Pair(e.type, e.message ?: ""))
+                    else
+                        errorSubject.onNext(Pair(ErrorType.UNKNOWN, e.message ?: ""))
+                }
+            })
+    }
 
-	override fun pause() {
-		if (status != LoadStatus.IN_PROGRESS) return
-		status = LoadStatus.PAUSE
-		loader?.isPaused = true
-	}
+    override fun pause() {
+        if (status != LoadStatus.IN_PROGRESS) return
+        status = LoadStatus.PAUSE
+        loader?.isPaused = true
+    }
 
-	override fun resume() {
-		if (status != LoadStatus.PAUSE) return
-		status = LoadStatus.IN_PROGRESS
-		loader?.isPaused = false
-	}
+    override fun resume() {
+        if (status != LoadStatus.PAUSE) return
+        status = LoadStatus.IN_PROGRESS
+        loader?.isPaused = false
+    }
 
-	override fun stop() {
-		loadingDis?.dispose()
-		loader?.stop()
-		loader = null
-		status = LoadStatus.CANCEL
-	}
+    override fun stop() {
+        loadingDis?.dispose()
+        loader?.stop()
+        loader = null
+        status = LoadStatus.CANCEL
+    }
 
-	override fun toString(): String {
-		val folder = File(meta.savingFolder, meta.namePrefix + (meta.fileName ?: id) + meta.namePostfix)
-		return "Item(url=$url, path=${folder.path}, id=$id)"
-	}
+    override fun toString(): String {
+        val folder = File(meta.savingFolder, meta.namePrefix + (meta.fileName ?: id) + meta.namePostfix)
+        return "Item(url=$url, path=${folder.path}, id=$id)"
+    }
 
-	override fun isExist(): Boolean {
-		val folder = File(meta.savingFolder)
-		if (folder.exists() && folder.isDirectory) {
-			val x = folder.listFiles()?.find {
-				it.nameWithoutExtension == meta.namePrefix + (meta.fileName ?: id) + meta.namePostfix
-			}
+    override fun isExist(): Boolean {
+        val folder = File(meta.savingFolder)
+        if (folder.exists() && folder.isDirectory) {
+            val x = folder.listFiles()?.find {
+                it.nameWithoutExtension == meta.namePrefix + (meta.fileName ?: id) + meta.namePostfix
+            }
 
-			return x != null
-		}
-		return false
-	}
+            return x != null
+        }
+        return false
+    }
 }
 
 /**
